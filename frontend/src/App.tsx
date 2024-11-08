773fe7ef4d0e6dfab38fc30eedb96622bd818a29
Komaccyau <eishisasso709@gmail.com>
2024-11-08 17:13:06 +0900

    Vite,yarnインストール

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home'; // ホームページコンポーネント
-import Quiz from './pages/Quiz'; // クイズページコンポーネント
-import NotFound from './pages/NotFound'; // 404ページコンポーネント
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme'; // MUIテーマ設定
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App: React.FC = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/quiz" component={Quiz} />
-          <Route component={NotFound} /> {/* 404ページ */}
-        </Switch>
-      </Router>
-    </ThemeProvider>
-  );
-};
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
