d0de9dc37d1b2d921bde8088a65eae30e8b35b06
Komaccyau <eishisasso709@gmail.com>
2024-11-21 14:01:07 +0900

    config

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,11 +5,11 @@ import google.generativeai as genai
 
 # .envファイルの読み込み
 load_dotenv()
- 
+
 # API-KEYの設定
-GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
- 
+
 gemini_pro = genai.GenerativeModel("gemini-pro")
 prompt = "質問内容を記載"
 response = gemini_pro.generate_content(prompt)
