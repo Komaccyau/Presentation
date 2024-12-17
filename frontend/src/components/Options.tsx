8213c3e35f32754dfbd9ef9ed9ecd62bd97cf40d
Komaccyau <eishisasso709@gmail.com>
2024-12-17 14:56:32 +0900

    あああ

--- frontend/src/components/Options.tsx
+++ frontend/src/components/Options.tsx
@@ -9,10 +9,9 @@ interface OptionsProps {
 
 const Options: React.FC<OptionsProps> = ({ toggleApiUsage, useDummyApi }) => {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'space-around', padding: '1vh' }}>
-      <TextField sx={{ width: '30vw' }} variant="outlined" placeholder="オプションを入力" />
-      <TextField sx={{ width: '30vw' }} variant="outlined" placeholder="オプションを入力" />
-      
+    <Box sx={{ marginLeft: '20vh',width: '80vw' , display: 'flex', justifyContent: 'space-around', padding: '1vh' }}>
+      <TextField sx={{ width: '30vw' }} variant="outlined" placeholder="返答を表示" />
+      <TextField sx={{ width: '30vw' }} variant="outlined" placeholder="返答を表示" />
       <Button variant="contained" onClick={toggleApiUsage}>
         {useDummyApi ? 'チャットAPIを使用' : 'ダミーAPIを使用'}
       </Button>
