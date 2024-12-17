84c7636202702666d7a2206114856511da143f08
Komaccyau <eishisasso709@gmail.com>
2024-12-17 13:25:29 +0900

    aaa

--- backend/app/global.d.ts
+++ backend/app/global.d.ts
@@ -1,15 +1,33 @@
 // global.d.ts
+
 interface Window {
     SpeechRecognition: typeof SpeechRecognition;
-    webkitSpeechRecognition: typeof SpeechRecognition;
-  }
-  
-  interface SpeechRecognitionEvent {
+    webkitSpeechRecognition: typeof SpeechRecognition; // Safariなどのブラウザ用
+}
+
+interface SpeechRecognition {
+    new (): SpeechRecognition;
+    start(): void;
+    stop(): void;
+    onresult: (event: SpeechRecognitionEvent) => void;
+    onerror: (event: SpeechRecognitionError) => void;
+}
+
+interface SpeechRecognitionEvent {
     results: SpeechRecognitionResultList;
-  }
-  
-  interface SpeechRecognitionResult {
-    isFinal: boolean;
-    [index: number]: SpeechRecognitionAlternative;
-  }
-  
+}
+
+interface SpeechRecognitionResult {
+    readonly isFinal: boolean;
+    readonly alternatives: SpeechRecognitionAlternative[];
+}
+
+interface SpeechRecognitionAlternative {
+    readonly transcript: string;
+    readonly confidence: number;
+}
+
+interface SpeechRecognitionError {
+    error: string;
+    message: string;
+}
