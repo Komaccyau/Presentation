2703801571114020a678423d731eec2304c837e0
Komaccyau <eishisasso709@gmail.com>
2025-01-10 11:53:31 +0900

    a

--- frontend/src/components/Options.tsx
+++ frontend/src/components/Options.tsx
@@ -10,8 +10,6 @@ interface OptionsProps {
 const Options: React.FC<OptionsProps> = ({ toggleApiUsage, useDummyApi }) => {
   return (
     <Box sx={{ marginLeft: '20vh',width: '80vw' , display: 'flex', justifyContent: 'space-around', padding: '1vh' }}>
-      {/* <TextField sx={{ width: '30vw' }} variant="outlined" placeholder="返答を表示" />
-      <TextField sx={{ width: '30vw' }} variant="outlined" placeholder="返答を表示" /> */}
       <Button variant="contained" onClick={toggleApiUsage}>
         {useDummyApi ? '中学生レベル(ChatAPI)' : '高校生レベル(DummyAPI)'}
       </Button>
